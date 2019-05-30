4238811721593300000816ed520a23fce871a74c
jenyaefremova <jenaefremova@gmail.com>
2019-05-30 11:14:09 +0300

    добавлено автоматическое закрытие модального окна через 5 секунд

--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,18 @@ var button = document.querySelector('#button');
 var modal = document.querySelector('#modal');
 var close = document.querySelector('#close');
 
+// Функция для закрытия модального окна
+var closeModal = function() {
+  modal.classList.remove('modal-active');
+}
+
+// при клике на кнопку "Перезвоните мне", всплывает модальное окно
 button.addEventListener('click', function() {
   modal.classList.add('modal-active');
 });
 
-close.addEventListener('click', function() {
-  modal.classList.remove('modal-active');
-});
+// при коике на закрывающий крестик вызывается функция для закрытия модального окна
+close.addEventListener('click', closeModal);
+
+//функция для автоматического закрытия модального окна
+setTimeout(closeModal, 5000);
